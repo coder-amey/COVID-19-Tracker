eae1b311cc085335aa2c3f0a112c28215343d241
Amey Noolkar <fb4amey@gmail.com>
2020-03-27 00:19:41 +0530

    Removed the 'Recovered' column from the dataframe as it is no longer supported.

--- COVID19_visualize.py
+++ COVID19_visualize.py
@@ -40,17 +40,17 @@ def plot_growth(df, figure, title):
 	growth_rate_log.set_ylabel('Cases per day')
 	growth_rate_log.set_yscale('log')
 
-	lines = cases.plot(df['Date'], df['Confirmed'], 'yellow', df['Date'], df['Recovered'], 'green', df['Date'], df['Deaths'], 'red')
-	cases.legend(lines, ('Confirmed', 'Recovered', 'Dead'))
+	lines = cases.plot(df['Date'], df['Confirmed'], 'yellow', df['Date'], df['Deaths'], 'red')
+	cases.legend(lines, ('Confirmed',  'Dead'))
 	
-	lines = cases_log.plot(df['Date'], df['Confirmed'], 'yellow', df['Date'], df['Recovered'], 'green', df['Date'], df['Deaths'], 'red')
-	cases_log.legend(lines, ('Confirmed', 'Recovered', 'Dead'))
+	lines = cases_log.plot(df['Date'], df['Confirmed'], 'yellow', df['Date'], df['Deaths'], 'red')
+	cases_log.legend(lines, ('Confirmed',  'Dead'))
 
-	lines = growth_rate.plot(df['Date'], get_rate(df['Confirmed']), 'yellow', df['Date'], get_rate(df['Recovered']), 'green', df['Date'], get_rate(df['Deaths']), 'red')
-	growth_rate.legend(lines, ('Confirmed', 'Recovered', 'Dead'))
+	lines = growth_rate.plot(df['Date'], get_rate(df['Confirmed']), 'yellow', df['Date'], get_rate(df['Deaths']), 'red')
+	growth_rate.legend(lines, ('Confirmed',  'Dead'))
 
-	lines = growth_rate_log.plot(df['Date'], get_rate(df['Confirmed']), 'yellow', df['Date'], get_rate(df['Recovered']), 'green', df['Date'], get_rate(df['Deaths']), 'red')
-	growth_rate_log.legend(lines, ('Confirmed', 'Recovered', 'Dead'))
+	lines = growth_rate_log.plot(df['Date'], get_rate(df['Confirmed']), 'yellow', df['Date'], get_rate(df['Deaths']), 'red')
+	growth_rate_log.legend(lines, ('Confirmed',  'Dead'))
 
 	figure.tight_layout(pad = 3.0, rect = [0, 0, 1, 0.94])
 	chart.show()
@@ -58,7 +58,7 @@ def plot_growth(df, figure, title):
 if __name__ == "__main__":
 	print("\t\tCOVID-19 Growth Visualization\n")
 	country = input("Plot COVID-19 Growth trends for (enter the name of the country or enter \'Global\' for worldwide trends):\n").lower()
-	COVID19_growth = chart.figure(figsize = (28, 14), dpi = 75, facecolor = 'white')
+	COVID19_growth = chart.figure(figsize = (28, 14), dpi = 75)
 
 	if(country == "global"):
 		COVID19_global_df = data.read_csv("https://raw.githubusercontent.com/datasets/covid-19/master/data/worldwide-aggregated.csv", parse_dates = ['Date'])
@@ -76,6 +76,6 @@ if __name__ == "__main__":
 			quit()
 
 	print("\nGenerating trends...")
-	print("Current Tally:\t{}\nConfirmed: {}\tRecovered: {}\tDead: {}".format(country.title(), tally['Confirmed'], tally['Recovered'], tally['Deaths']))
+	print("Current Tally:\t{}\nConfirmed: {}\tDead: {}".format(country.title(), tally['Confirmed'], tally['Deaths']))
 	print("Mortality rate: {}%".format(str.format('{0:.3f}', 100 * tally['Deaths'] / tally['Confirmed'])))
 	plot_growth(df, COVID19_growth, country)
